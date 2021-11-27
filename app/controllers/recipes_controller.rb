cf72385c0eebc8528193fa9f95dfbaf07a3a0438
ThaiDelgado <thaissadelgado@gmail.com>
2021-11-27 14:37:19 -0300

    add instance variable

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,9 @@
 class RecipesController < ApplicationController
     def index
+        @recipes = ["Pato no Tucupi", "Vatapá", "Arroz Paraense", "Caruru", "Açaí"] #@recipes é uma variável de instância que irá receber todas as 
+                                                                            #informações vindas do banco de dados. A views enxerga a variável de instância.
+                                                                            #Neste momento estamos atribuindo valores a esta variável, os quais serão renderizados na view.
+                                                                            #Em um outro momento passaremos informações vindo ddireto do banco de dados                  
         
     end
 end
