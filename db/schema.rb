51f518c9100780cc736a1998f6a93aafaf4794c9
ThaiDelgado <thaissadelgado@gmail.com>
2021-12-05 13:56:40 -0300

    add novos campos na tabela Recipes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_03_005713) do
+ActiveRecord::Schema.define(version: 2021_12_05_143211) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2021_12_03_005713) do
     t.integer "calories"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "prepare_mode"
+    t.decimal "cost"
   end
 
 end
