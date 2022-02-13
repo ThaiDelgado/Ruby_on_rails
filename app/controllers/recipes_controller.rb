9f869ba3fded3eb8a342df47a364cd13932cc8c4
ThaiDelgado <thaissadelgado@gmail.com>
2022-02-12 23:07:50 -0300

    Cria migrate e add arquivo model

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,14 +1,19 @@
 class RecipesController < ApplicationController
-    def index
-        @recipes = Recipe.all
-                                                                            
+  def index
+    @recipes = Recipe.all # Trazendo todas as receitas direto do banco de dados para a variável
+  end
 
+#   def show
+#     ender json: @recipe
+#   end
 
+#   private
 
-    end
+#   def set_recip
+#     @recipe = Recipe.find(:id)
+#   end
 end
-# @recipes é uma variável de instância que irá receber todas as 
+# @recipes é uma variável de instância que irá receber todas as
 # informações vindas do banco de dados. A views enxerga a variável de instância.
 # Neste momento estamos atribuindo valores a esta variável, os quais serão renderizados na view.
-# Em um outro momento passaremos informações vindo ddireto do banco de dados                  
-    
+# Em um outro momento passaremos informações vindo ddireto do banco de dados.
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-
-  root 'recipes#index'
+  root 'recipes#index' # setando à aplicação que index é a primeira página que deverá ser renderizada
   resources :recipes
 end
