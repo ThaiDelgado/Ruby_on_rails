e0bb42a64df38992a533391c8bcddb79bb4e0049
ThaiDelgado <thaissadelgado@gmail.com>
2022-02-13 13:41:23 -0300

    cria e add um helper

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,11 @@
 module RecipesHelper
+
+  def format_calories(recipe)
+    if recipe.ligth?
+      content_tag(:strong, 'Ligth - Menos de 100 Calorias!')
+    else
+      recipe.calories
+    end
+  end
+
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,8 @@
 class Recipe < ApplicationRecord
 
-    
+  # retorna um boolean
+  def ligth?
+    recipe.calories < 100
+  end
+
 end
